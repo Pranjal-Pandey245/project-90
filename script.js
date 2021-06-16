92f8cf44bc4717e2a4a6d683b263c1900e390c33
Pranjal-Pandey245 <81172387+Pranjal-Pandey245@users.noreply.github.com>
2021-06-16 12:01:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function Dis_play1(){
+in_p_1=document.getElementById(id="in_1").value;
+document.getElementById("p1").innerHTML="Player 1 name- "+in_p_1;
+}
+
+function Dis_play2(){
+    in_p_2=document.getElementById(id="in_2").value;
+    document.getElementById("p2").innerHTML="Player 2 name- "+in_p_2;
+}
+
+function next_page(){
+    window.location="main.html";
+}
